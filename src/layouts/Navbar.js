31061ca7250c7e307e679b8b41aff86e61e5f5e4
Ranilo Cabral <ranilocabral@gmail.com>
2024-03-24 12:42:14 -0300

    menu hamburguer adicionado

--- src/layouts/Navbar.js
+++ src/layouts/Navbar.js
@@ -1,33 +1,44 @@
-import { Link } from 'react-router-dom'
-
-import Container from './Container'
-
-import styles from './Navbar.module.css'
-import logo from '../img/costs_logo.png'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FaBars } from 'react-icons/fa';
+import Container from './Container';
+import styles from './Navbar.module.css';
+import logo from '../img/costs_logo.png';
 
 function Navbar() {
-    return(
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
         <nav className={styles.navbar}>
             <Container>
-                <Link to="/"><img src={logo} alt='Costs'/></Link>
-                <ul className={styles.list}>
+                <div className={styles.logo}>
+                    <Link to="/"><img src={logo} alt='Costs'/></Link>
+                </div>
+                <div className={styles.menuToggle} onClick={toggleMenu}>
+                    <FaBars />
+                </div>
+                <ul className={`${styles.list} ${isOpen ? styles.showMenu : ''}`}>
                     <li className={styles.item}>
-                    <Link to="/" exact>Home</Link>
+                        <Link to="/" exact>Home</Link>
                     </li>
                     <li className={styles.item}>
-                    <Link to="/projects" exact>Projetos</Link>
+                        <Link to="/projects" exact>Projetos</Link>
                     </li>
                     <li className={styles.item}>
-                    <Link to="/company">Empresa</Link>
+                        <Link to="/company">Empresa</Link>
                     </li>
                     <li className={styles.item}>
-                    <Link to="/contact">Contato</Link>
+                        <Link to="/contact">Contato</Link>
                     </li>
                 </ul>
             </Container>
         </nav>
-    )
+    );
 }
 
-export default Navbar
-
+export default Navbar;
+    
